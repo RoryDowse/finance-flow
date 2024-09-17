f19012a72e3668f6304232e1e4340e441d5d4d07
Rory Dowse <rorydowse@hotmail.com>
2024-09-17 10:35:57 -0700

    update models index.ts

--- server/src/models/index.ts
+++ server/src/models/index.ts
@@ -1,2 +1,29 @@
-import sequelize from '../config/connection.js';
-// Import model factories here
+import dotenv from 'dotenv';
+dotenv.config();
+
+import { Sequelize } from 'sequelize';
+import { UserFactory } from './user';
+import { IncomeFactory } from './income';
+import { ExpenseFactory } from './expense';
+
+const sequelize = process.env.DB_URL
+  ? new Sequelize(process.env.DB_URL)
+  : new Sequelize(process.env.DB_NAME || '', process.env.DB_USER || '', process.env.DB_PASSWORD, {
+      host: 'localhost',
+      dialect: 'postgres',
+      dialectOptions: {
+        decimalNumbers: true,
+      },
+    });
+
+const User = UserFactory(sequelize);
+const Income = IncomeFactory(sequelize);
+const Expense = ExpenseFactory(sequelize);
+
+User.hasMany(Income, { foreignKey: 'userId', as: 'incomes' });
+Income.belongsTo(User, { foreignKey: 'userId', as: 'user' });
+
+User.hasMany(Expense, { foreignKey: 'userId', as: 'expenses' });
+Expense.belongsTo(User, { foreignKey: 'userId', as: 'user' });
+
+export { sequelize, User, Income, Expense };
