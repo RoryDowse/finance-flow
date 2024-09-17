ae2ac1a6c1cfe8870d559d7ad35250ad6b93a93e
Rory Dowse <rorydowse@hotmail.com>
2024-09-16 21:31:14 -0700

    server-side update

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,7 +1,11 @@
-// TODO: Create Sequelize connector from models folder and import in to connect to postgres server. 
+const forceDatabaseRefresh = false;
+
+import dotenv from 'dotenv';
+dotenv.config();
 
 import express from 'express';
 import routes from './routes/index.js';
+import { sequelize } from './models/index.js';
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -12,6 +16,9 @@ app.use(express.static('../client/dist'));
 app.use(express.json());
 app.use(routes);
 
-app.listen(PORT, () => {
-  console.log(`Server is listening on port ${PORT}`);
+sequelize.sync({force: forceDatabaseRefresh}).then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
+  });
 });
+
