668ce2faefedc2d36b53c84d897bb7c4f454b528
Cameron Barfuss <Cameron.barfuss@gmail.com>
2024-09-18 11:16:50 -0600

    Add some more functionality to the Travel Page, added buttons for 1-3-5-10 years respectively

--- client/src/pages/Travel.tsx
+++ client/src/pages/Travel.tsx
@@ -1,21 +1,25 @@
 // src/components/Travel.tsx
 import React, { useState, useEffect } from 'react';
-import { Currency } from './interfaces/Currency.Interface';
+import { Currency } from './interfaces/Currency.Interface.tsx';
 
 const Travel: React.FC = () => {
-    
+   
     const [currency, setCurrency] = useState<string>('USD');
-    const [convertedAmount, setConvertedAmount] = useState<number | null>(null);
-    
-    const exchangeKey = process.env.EXCHANGE_API_KEY;
+    const [convertedAmount, setConvertedAmount] = useState<number>();
+    const [numberOfYears, setNumberOfYears] = useState<number>(1);
+   
+    const exchangeKey = import.meta.env.VITE_EXCHANGE_API_KEY;
+
 
     const searchCurrencyType = async (currency: string) => {
         try {
             const response = await fetch(`https://v6.exchangerate-api.com/v6/${exchangeKey}/latest/${currency}`)
 
+
             if (!response.ok) {
                 throw new Error('invalid API response, check the network tab');
-            } 
+            }
+
 
             const data: Currency = await response.json();
 
@@ -35,11 +39,46 @@ const Travel: React.FC = () => {
         }
     };
 
+    // This function will take the number of years the user chooses and convert the numbers displayed for convertedAmount
+    const currencyPerYear = (numberOfYears: number) => {
+        const newConvertedAmount = numberOfYears * convertedAmount;
+        newCurrencyAmount(newConvertedAmount);
+    }
+
+
+    // Handles the Search bar Input, setting Currency to be whatever they type.
+    const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setCurrency(event.target.value.toUpperCase());
+    };
+
+    // Handles submission for search field
+    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        searchCurrencyType(currency);
+    };
 
     return (
         <section>
+            <form onSubmit={handleSubmit}>
+                <input
+                type="text"
+                value={currency}
+                onChange={handleInputChange}
+                placeholder="Enter Currency Type"
+                />
+                <button type="submit">Search</button>
+            </form>
             <div>
-                <h2>Currency Exchange</h2>
+                <h2>Currency Exchange Rates</h2>
+                <p>
+                    Your Cashflow Converted from USD to {currency} is {convertedAmount?.toFixed(2)} for {numberOfYears}
+                </p>
+            </div>
+            <div>
+                <button onClick={() => currencyPerYear(1)}>1 Year</button>
+                <button onClick={() => currencyPerYear(3)}>3 Years</button>
+                <button onClick={() => currencyPerYear(5)}>5 Years</button>
+                <button onClick={() => currencyPerYear(10)}>10 Years</button>
             </div>
         </section>
     );
