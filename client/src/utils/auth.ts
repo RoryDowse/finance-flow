bad8936b9362009facd21251e9f45d4f8b1693cc
Cameron Barfuss <Cameron.barfuss@gmail.com>
2024-09-19 12:17:13 -0600

    add userApi.tsx file and /utils/auth.ts file, allowing for user authentication

---
+++ client/src/utils/auth.ts
@@ -0,0 +1,43 @@
+import { JwtPayload, jwtDecode } from 'jwt-decode';
+
+
+class AuthService {
+
+  // Creates a new token variable that gets the JWT token if there is one, returns a boolean for whether or not the token is expired or active
+  loggedIn() {
+    const token = this.getToken();
+    return !!token && !this.isTokenExpired(token);
+  }
+ 
+  // Checks if the token given is expired or not using jwtDecode
+  isTokenExpired(token: string) {
+    try {
+      const decoded = jwtDecode<JwtPayload>(token);
+      if (decoded?.exp && decoded?.exp < Date.now() / 1000) {
+        return true;
+      }
+    } catch (err) {
+      return false;
+    }
+  }
+
+  // Gets the token saved in local storage and returns it
+  getToken(): string {
+    const loggedUser = localStorage.getItem('id_token') || '';
+    return loggedUser;
+  }
+
+  // Sets the token in local storage and reassigns the users page to the "Board" page
+  login(idToken: string) {
+    localStorage.setItem('id_token', idToken);
+    window.location.assign('/');
+  }
+
+  // Removes the token from local storage and returns the user to the "Board" page
+  logout() {
+    localStorage.removeItem('id_token');
+    window.location.assign('/');
+  }
+}
+
+export default new AuthService();
