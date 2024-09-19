55bdf8a5dfb3263dc201aab3256fa00d48d3d213
Cameron Barfuss <Cameron.barfuss@gmail.com>
2024-09-19 12:29:33 -0600

    add base setup for the Login Page

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,11 +1,56 @@
-// src/components/Login.tsx
-import React from 'react';
+import React, { useState, FormEvent, ChangeEvent } from "react";
+import Auth from '../utils/auth';
+import { login } from "../api/authApi";
+
 
 const Login: React.FC = () => {
+
+    const [loginData, setLoginData] = useState({
+        username: '',
+        password: ''
+      });
+   
+      const handleChange = (e: ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
+        const { name, value } = e.target;
+        setLoginData({
+          ...loginData,
+          [name]: value
+        });
+      };
+   
+      const handleSubmit = async (e: FormEvent) => {
+        e.preventDefault();
+        try {
+          const data = await login(loginData);
+          Auth.login(data.token);
+        } catch (err) {
+          console.error('Failed to login', err);
+        }
+      };
+     
     return (
         <div>
             <h1>Login Page</h1>
-            {/* Add your login form or content here */}
+            <div>
+                <form onSubmit={handleSubmit}>
+                    <h1>Login</h1>
+                    <label >Username</label>
+                    <input
+                    type='text'
+                    name='username'
+                    value={loginData.username || ''}
+                    onChange={handleChange}
+                    />
+                <label>Password</label>
+                    <input
+                    type='password'
+                    name='password'
+                    value={loginData.password || ''}
+                    onChange={handleChange}
+                    />
+                    <button type='submit'>Submit Form</button>
+                </form>
+            </div>
         </div>
     );
 };
