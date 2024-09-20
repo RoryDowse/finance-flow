3cd7592d24ce4e210db1dbf86264d7f8c708fee1
Cameron Barfuss <Cameron.barfuss@gmail.com>
2024-09-20 14:24:21 -0600

    add corrected files

--- server/dist/config/connection.js
+++ server/dist/config/connection.js
@@ -1,13 +1,6 @@
 import { Sequelize } from 'sequelize';
 import dotenv from 'dotenv';
 dotenv.config();
-// export const sequelize = new Sequelize('cashflow_db', 'postgres', 'new-password', {
-//     host: 'localhost',
-//     dialect: 'postgres',
-//     dialectOptions: {
-//       decimalNumbers: true,
-//     },
-//   });
 export const sequelize = process.env.DB_URL
     ? new Sequelize(process.env.DB_URL)
     : new Sequelize(process.env.DB_NAME || '', process.env.DB_USER || '', process.env.DB_PASSWORD, {
@@ -17,7 +10,3 @@ export const sequelize = process.env.DB_URL
             decimalNumbers: true,
         },
     });
-console.log('DB_NAME:', process.env.DB_NAME);
-console.log('DB_USER:', process.env.DB_USER);
-console.log('DB_PASSWORD:', process.env.DB_PASSWORD);
-console.log('DB_URL:', process.env.DB_URL);
