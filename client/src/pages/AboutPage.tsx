d8135976bdcb7c1eeba3c4f2562792128bbead57
Rory Dowse <rorydowse@hotmail.com>
2024-09-21 09:21:17 -0700

    add About styles

--- client/src/pages/AboutPage.tsx
+++ client/src/pages/AboutPage.tsx
@@ -1,9 +1,9 @@
-// src/components/About.tsx
+import './AboutPage.css';
 const About = () => {
     return (
         <div>
-            <h1>About</h1>
-            <p>FinanceFlow aims to help users understand their cashflow in order to make informed decisions about investments and travel. Users can track their income and expenses, view financial projections, and analyze potential returns on stocks or currency exchanges. The app calculates users' cashflow and projects their potential returns using stock market and currency data, helping users prioritize their spending.</p>
+            <h2 className="text-center">About</h2>
+            <p className="about-info">FinanceFlow helps users understand their cashflow in order to make informed decisions about investments and travel. Users can track their income and expenses, view financial projections, and analyze potential returns on stocks or currency exchanges. The app calculates users' cashflow and projects their potential returns using stock market and currency data, helping users prioritize their spending.</p>
         </div>
     );
 };
