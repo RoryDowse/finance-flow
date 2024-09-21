6989173be9b3bc0b8b81fae1470c5660798374f9
Cameron Barfuss <Cameron.barfuss@gmail.com>
2024-09-20 19:07:29 -0600

    add footer component, add styling to all pages

---
+++ client/src/components/Footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return (
+        <div className="footer">
+            <p>Created by: Rory Dowse and Cameron Barfuss</p>
+        </div>
+      );
+    };
+    
+    export default Footer;
